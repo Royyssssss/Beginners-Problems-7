d027a40b2908adf6412d96da9708a8cbb8108e89
Royyssssss <137406143+Royyssssss@users.noreply.github.com>
2024-07-04 14:43:04 +1200

    Create Royhw.py

---
+++ Royhw.py
@@ -0,0 +1,18 @@
+seen = set()
+print("Please enter 7 numbers:")
+for i in range(7):
+    num = int(input())
+    if num in seen:
+        print('YES')
+    else:
+        print('NO')
+        seen.add(num)
+
+
+test_str = str(input())
+count = 0
+ 
+for i in test_str:
+    if i == 'e':
+        count = count + 1
+print(count)
